80d67f59a2f556e8671ac0186f226adef7d7e54c
bertek <81099969+albertoecf@users.noreply.github.com>
2023-12-19 20:08:48 +0100

    define conftest path and mock report view

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 
 app = FastAPI()
-
+test_file_path = Path( "tests/test_report/test_report.csv") 
 
 @app.get("/")
 def hello_handler():
     return {"msg": "Hello, World!"}
+
+#todo define a proper view
+@app.get("/report")
+def read_report():   
+    return FileResponse(test_file_path)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 import pytest
 from fastapi import FastAPI
 from starlette.testclient import TestClient
+import sys
+from pathlib import Path
 
+# Add the project's root directory to sys.path
+project_root = Path(__file__).resolve().parents[1]
+sys.path.append(str(project_root))
 from src.main import app as orig_app
 
 
