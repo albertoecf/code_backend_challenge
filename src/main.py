badaed45d06629ff5344d3de25baba659315414a
bertek <81099969+albertoecf@users.noreply.github.com>
2023-12-21 12:05:41 +0100

    return csv file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-from src.controllers import GenerateReport
-from src.app_config import result_file_path, bookings_file_path, chart_of_accounts_file_path
+from controllers import GenerateReport
+from app_config import (
+    result_file_path,
+    bookings_file_path,
+    chart_of_accounts_file_path,
+)
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
 @app.get("/")
@@ -25,35 +29,18 @@ def read_report():
         new_period = {"year": 2020, "month": 6}
         old_period = {"year": 2020, "month": 5}
 
-        # Generate report for the specified period
-        new_period_income_statement = report_generator.generate_report(new_period)
-        old_period_income_statement = report_generator.generate_report(old_period)
-
-        # Return the IncomeStatement as JSON
-        # todo: comparre both periods
-        comparison_results = report_generator.compare_income_statements(
-            new_period_income_statement, old_period_income_statement
+        # Generate and save the report
+        output_file_path = report_generator.generate_and_save_report(
+            new_period, old_period
         )
-        # Construct the final report dictionary
-        final_report = {
-            "June, 2020": new_period_income_statement.dict(),
-            "May, 2020": old_period_income_statement.dict(),
-            "June vs May, 2020 (Abs)": comparison_results,
-        }
 
-        # Save the final report to a JSON file
-        import json
-        output_file_path = "final_report.json"
-        with open(output_file_path, "w") as output_file:
-            json.dump(final_report, output_file)
-
-        # Return the final report file as a downloadable response
+        # Return the final report file as a downloadable response with status code 200
         return FileResponse(
             output_file_path,
-            media_type="application/json",
-            filename="final_report.json",
+            media_type="text/csv",  # Use CSV media type
+            filename="final_report.csv",
+            status_code=200,
         )
-
     except ValueError as ve:
         # Handle validation errors and return an error response
         return JSONResponse(content={"error": str(ve)}, status_code=400)
@@ -64,7 +51,7 @@ def read_report():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "src.main:app",
+        "main:app",
         host=os.getenv("APP_HOST", "0.0.0.0"),
         port=int(os.getenv("APP_PORT", 8000)),
         workers=int(os.getenv("APP_WORKERS", 1)),
