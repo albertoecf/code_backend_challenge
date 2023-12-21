68ad65271fca2fd38e9de3ef854402bdc5f5a2f8
bertek <81099969+albertoecf@users.noreply.github.com>
2023-12-21 12:50:00 +0100

    Deploy report enhancement and docs (#6)
    * fix docker command (#2)
    
    * Helu 02 report Adding MVC and prepare for deployment (#3)
    
    * feat: implementing MVC pending improvements and prepare for deployment
    
    * Helu 03 improvements (#5)
    
    * returning file as response
    
    * return csv file
    
    * adding docs

--- src/main.py
+++ src/main.py
@@ -1,55 +1,72 @@
 import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 from src.controllers import GenerateReport
-from src.app_config import result_file_path, bookings_file_path, chart_of_accounts_file_path
-from fastapi.responses import JSONResponse
+from src.app_config import (
+    bookings_file_path,
+    chart_of_accounts_file_path,
+)
 
-app = FastAPI()
+app = FastAPI(
+    title="Helu Financial Report API",
+    description="API for generating and downloading financial reports",
+    version="1.0.0",
+)
 
+# Initialize the controller with necessary file paths
+report_generator = GenerateReport(bookings_file_path, chart_of_accounts_file_path)
+
+
+# Dependency to use in the path operation
+def get_report_generator():
+    return report_generator
 
 @app.get("/")
 def hello_handler():
     return {"msg": "Hello, World!"}
 
-
-@app.get("/report")
+@app.get(
+    "/report",
+    tags=["report"],
+    summary="Download Financial Report",
+    response_description="The generated financial report in CSV format",
+)
 def read_report():
-    try:
-        # Initialize the controller with necessary file paths
-        report_generator = GenerateReport(
-            bookings_file_path, chart_of_accounts_file_path
-        )
+    """
+    Generate and download the financial report.
 
+    Warning:
+        Args are hardcoded, we could improve our endpoint to accept such parameters
+        new_period (dict): Dictionary containing 'year' and 'month' for the new period.
+        old_period (dict): Dictionary containing 'year' and 'month' for the old period.
+
+    Returns:
+        FileResponse: The generated financial report in CSV format.
+    """
+    try:
+        # Generate and save the report
         new_period = {"year": 2020, "month": 6}
         old_period = {"year": 2020, "month": 5}
 
-        # Generate report for the specified period
-        new_period_income_statement = report_generator.generate_report(new_period)
-        old_period_income_statement = report_generator.generate_report(old_period)
-
-        # Return the IncomeStatement as JSON
-        # todo: comparre both periods
-        comparison_results = report_generator.compare_income_statements(
-            new_period_income_statement, old_period_income_statement
+        output_file_path = report_generator.generate_and_save_report(
+            new_period, old_period
         )
 
-        # Return the comparison results as JSON
-        return JSONResponse(
-            content=[
-                new_period_income_statement.dict(),
-                old_period_income_statement.dict(),
-                comparison_results,
-            ]
+        # Return the final report file as a downloadable response with status code 200
+        return FileResponse(
+            output_file_path,
+            media_type="text/csv",  # Use CSV media type
+            filename="final_report.csv",
+            status_code=200,
         )
-        # return JSONResponse(content=new_period_income_statement.dict())
-
     except ValueError as ve:
         # Handle validation errors and return an error response
-        return JSONResponse(content={"error": str(ve)}, status_code=400)
+        raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
         # Handle other exceptions and return an error response
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 
 if __name__ == "__main__":
